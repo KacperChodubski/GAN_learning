4a47ee88803453157fab58dc0a7d26d0e2fe32f8
Kacper <kaper02@o2.pl>
2023-04-13 20:42:00 +0200

    bug fix

--- WGANGP/utils.py
+++ WGANGP/utils.py
@@ -3,12 +3,13 @@ import torch.nn as nn
 
 def gradient_penalty(critic, real, fake, device="cpu"):
     BATCH_SIZE, C, H, W = real.shape
-    epsilon = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, C, H, W).to(device)
+    alpha = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, C, H, W).to(device)
+    interpolated_images = real * alpha + fake * (1 - alpha)
 
-    interpolated_images = real * epsilon + fake * (1 - epsilon)
-
+    # Calculate critic scores
     mixed_scores = critic(interpolated_images)
 
+    # Take the gradient of the scores with respect to the images
     gradient = torch.autograd.grad(
         inputs=interpolated_images,
         outputs=mixed_scores,
@@ -16,8 +17,7 @@ def gradient_penalty(critic, real, fake, device="cpu"):
         create_graph=True,
         retain_graph=True,
     )[0]
-
-    gradient.view(gradient.shape[0], -1)
+    gradient = gradient.view(gradient.shape[0], -1)
     gradient_norm = gradient.norm(2, dim=1)
     gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
     return gradient_penalty
